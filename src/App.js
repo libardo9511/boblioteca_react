851b2f79c31a8c35423521cb2f80482474e3e64f
Libardo Andrés Mora <libardo9511@hotmail.com>
2020-05-27 09:57:25 -0500

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Books from "./pages/books/books-items.page";
+import Readers from "./pages/readers/readers-items.page";
+import ReadersType from "./pages/reader_type/reader_type.page";
+import ReadersTypeEdit from "./pages/reader_type/reader_type_edit.page";
+
+import NavBar from "./components/navigate/navigate.component";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar></NavBar>
+      <div className="container p-4">      
+        <Switch>
+          <Route path="/books" exact component={Books}></Route> 
+          <Route path="/books/:id" exact component={Books}></Route>                             
+          <Route path="/readers" exact component={Readers}></Route>
+          <Route path="/readerstype" exact component={ReadersType}></Route>
+          <Route path="/readerstype/:id" exact component={ReadersTypeEdit}></Route>   
+          <Redirect from="/" to="/books"></Redirect>       
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
